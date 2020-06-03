4b4542818750f018a832e769b28813b06cf4a3aa
Abhishek Surve <abhishek.sur@media.net>
2020-06-03 14:12:38 +0530

    W1:Q3:Two City Scheduling

--- WEEK1/TwoCityScheduling.java
+++ WEEK1/TwoCityScheduling.java
@@ -7,19 +7,16 @@ public class TwoCityScheduling {
      */
     class Solution {
         public int twoCitySchedCost(int[][] costs) {
-            Arrays.sort(costs, (a, b) -> {
-                return a[0] - a[1] - (b[0] - b[1]);
-            });
-
-            int sum = 0;
-            for (int i = 0; i < costs.length; i++) {
-                if (i < costs.length / 2) {
-                    sum += costs[i][0];
-                } else {
-                    sum += costs[i][1];
+            Arrays.sort(costs, new Comparator<int[]>() {
+                public int compare(int[] a, int[] b) {
+                    return (a[1] - a[0]) - (b[1] - b[0]);
                 }
+            });
+            int cost = 0;
+            for (int i = 0; i < costs.length / 2; i++) {
+                cost += costs[i][1] + costs[costs.length-i-1][0];
             }
-            return sum;
+            return cost;
         }
     }
 
